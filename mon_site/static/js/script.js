9327c7ce331ed698a58d96de36feb6cb27f34a64
mvoisin <mederic.medo@gmail.com>
2016-05-03 16:55:00 +0200

    jout de map

--- mon_site/static/js/script.js
+++ mon_site/static/js/script.js
@@ -1,1 +1,12 @@
 $(document).foundation();
+
+$(document).ready(function(){
+
+
+  var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+  L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(mymap);
+
+});
