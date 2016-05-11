2b5e6ab6cd150e54e1b294fcd510db349fcfa6dc
mvoisin <mederic.medo@gmail.com>
2016-05-11 14:03:27 +0200

    Commit après passage d'Alban

--- mon_site/static/js/script.js
+++ mon_site/static/js/script.js
@@ -3,7 +3,7 @@ $(document).foundation();
 $(document).ready(function(){
 
 
-  var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+  var mymap = L.map('mapid').setView([44.494203, 3.581269], 10);
   L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
