47d5794dfefb492f905cddb3e0538c66b37c40e1
Nadège Jahier <jahiernadege@yahoo.fr>
2016-05-17 14:03:38 +0200

    modif js carte page les tournees

--- mon_site/static/js/script.js
+++ mon_site/static/js/script.js
@@ -2,7 +2,6 @@ $(document).foundation();
 
 $(document).ready(function(){
 
-
   var mymap = L.map('mapid',
                     {scrollWheelZoom:false ,
                     dragging:false,
@@ -13,34 +12,33 @@ $(document).ready(function(){
     doubleClickZoom: false,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
   }).addTo(mymap);
-  var marker = L.marker([44.52063000, 3.49660000]).addTo(mymap).bindPopup("Mende");
-  var marker = L.marker([44.7269, 3.85472]).addTo(mymap).bindPopup("Langogne"); // Langogne
-  var marker = L.marker([44.8012469, 3.2754536]).addTo(mymap).bindPopup("St Chély d'Apcher"); // Saint Chély
-  var marker = L.marker([44.8172, 3.11806]).addTo(mymap).bindPopup("Fournels"); // Fournels
-  var marker = L.marker([44.5528, 3.28917]).addTo(mymap).bindPopup("Marvejols"); // Marvejols
-  var marker = L.marker([44.3091, 3.17264]).addTo(mymap).bindPopup("Massegros"); // Massegros
-  var marker = L.marker([44.1783, 3.42972]).addTo(mymap).bindPopup("Meyrueis"); // Meyrueis
-  var marker = L.marker([44.3198, 3.5990]).addTo(mymap).bindPopup("Florac"); // Florac
-  var marker = L.marker([44.3631, 3.74417]).addTo(mymap).bindPopup("Pont-de-Montvert"); // Pont-de-Monvert
-  var marker = L.marker([44.43840, 3.93288]).addTo(mymap).bindPopup("Villefort"); // Villefort
-  $(".leaflet-control-attribution").hide();
+
+  var villesEtapes = [
+    {"nom" : "Mende", "coord" : [44.52063000, 3.49660000]},
+    {"nom" : "Langogne", "coord" : [44.7269, 3.85472]},
+    {"nom" : "St Chély d'Apcher", "coord" : [44.8012469, 3.2754536]},
+    {"nom" : "Fournels", "coord" : [44.8172, 3.11806]},
+    {"nom" : "Marvejols", "coord" : [44.5528, 3.28917]},
+    {"nom" : "Massegros", "coord" : [44.3091, 3.17264]},
+    {"nom" : "Meyrueis", "coord" : [44.1783, 3.42972]},
+    {"nom" : "Florac", "coord" : [44.3198, 3.5990]},
+    {"nom" : "Pont-de-Montvert", "coord" : [44.3631, 3.74417]},
+    {"nom" : "Villefort", "coord" : [44.43840, 3.93288]},
+  ];
+
+  var mesWaypoints = []
+
+  for (var i = 0; i < villesEtapes.length; i++) {
+    L.marker(villesEtapes[i].coord).addTo(mymap).bindPopup(villesEtapes[i].nom);
+    mesWaypoints.push(villesEtapes[i].coord);
+  }
 
   L.Routing.control({
-  waypoints: [
-    L.latLng(44.52063000, 3.49660000),
-    L.latLng(44.7269, 3.85472),
-    L.latLng(44.8012469, 3.2754536),
-    L.latLng(44.8172, 3.11806),
-    L.latLng(44.5528, 3.28917),
-    L.latLng(44.3091, 3.17264),
-    L.latLng(44.1783, 3.42972),
-    L.latLng(44.3198, 3.5990),
-    L.latLng(44.3631, 3.74417),
-    L.latLng(44.43840, 3.93288),
-  ]
-}).addTo(mymap);
+      waypoints: mesWaypoints
+  }).addTo(mymap);
 
-$(".leaflet-control").hide();
+  $(".leaflet-control-attribution").hide();
+  $(".leaflet-control").hide();
 
 
 });
